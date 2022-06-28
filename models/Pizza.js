38a20e8a24357fec33e0c5b2165ee1e1f352e4d5
AndreBlankholm <ablankholm@gmail.com>
2022-06-28 15:08:28 -0500

    crud done

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,33 +1,23 @@
 const { Schema, model } = require('mongoose');
-//only worried about the schema constructor and model function
 
-
-
-// Creating The Schema
 const PizzaSchema = new Schema({
-
-    pizzaName: {
-        type: String
-    },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now    // setting the default javaScript field Date.now
-    },
-    size: {
-        type:String,
-        default: 'Large'
-    },
-    toppings: []
-
+  pizzaName: {
+    type: String
+  },
+  createdBy: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  size: {
+    type: String,
+    default: 'Large'
+  },
+  toppings: []
 });
 
-
-
-// Creating The Model
 const Pizza = model('Pizza', PizzaSchema);
 
-//export the Pizza Model
 module.exports = Pizza;
