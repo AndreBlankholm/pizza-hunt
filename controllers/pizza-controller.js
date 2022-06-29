2d6d9c43419d079590b2b84405ae4d79d42c989f
AndreBlankholm <ablankholm@gmail.com>
2022-06-29 15:58:19 -0500

    added comments

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,55 +4,73 @@ const pizzaController = {
   // get all pizzas        Get   http://localhost:3001/api/pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: 'comments', //pizza also populates comments
+        select: '-__v'   // The minus sign - in front of the field indicates that we don't want it to be returned.
+      })
+      .select('-__v')  //this put the sort in DESC order by the _id value
+      .sort({ _id: -1 })   //newest item returns first
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
-        res.sendStatus(400);
+        res.status(400).json(err);
       });
   },
+
 
   // get one pizza by id  Get http://localhost:3001/api/pizzas/<pizza-id-here>
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
-      .then(dbPizzaData => res.json(dbPizzaData))
+      .populate({
+        path: 'comments', //pizza also populates comments
+        select: '-__v' // The minus sign - in front of the field indicates that we don't want it to be returned.
+      })
+      .select('-__v')         //  //this put the sort in DESC order by the _id value
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
       .catch(err => {
         console.log(err);
-        res.sendStatus(400);
+        res.status(400).json(err);
       });
   },
 
   // createPizza    Post http://localhost:3001/api/pizzas
   createPizza({ body }, res) {
     Pizza.create(body)
-      .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => res.json(err));
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => res.json(err));
   },
 
   // update pizza by id  Put http://localhost:3001/api/pizzas/<pizza-id-here>
   updatePizza({ params, body }, res) {
     Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
-      .then(dbPizzaData => {
+      .then((dbPizzaData) => {
         if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
+          res.status(404).json({ message: "No pizza found with this id!" });
           return;
         }
         res.json(dbPizzaData);
       })
-      .catch(err => res.status(400).json(err));
+      .catch((err) => res.status(400).json(err));
   },
 
   // delete pizza  Delete  localhost:3001/api/pizzas/<pizza-id-here>
   deletePizza({ params }, res) {
     Pizza.findOneAndDelete({ _id: params.id })
-      .then(dbPizzaData => {
+      .then((dbPizzaData) => {
         if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
+          res.status(404).json({ message: "No pizza found with this id!" });
           return;
         }
         res.json(dbPizzaData);
       })
-      .catch(err => res.status(400).json(err));
-  }
+      .catch((err) => res.status(400).json(err));
+  },
 };
 
 module.exports = pizzaController;
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require('mongoose');
 
+
 const PizzaSchema = new Schema({
   pizzaName: {
     type: String
@@ -9,13 +10,32 @@ const PizzaSchema = new Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal)  //getters method/ middleware for date using the utils/dateFormat.js file
   },
   size: {
     type: String,
     default: 'Large'
   },
-  toppings: []
+  comments: [
+    {
+      type: Schema.Types.ObjectId,   // (18.2.4) We'll need to update it to refer to the Comment type that we've created.
+      ref: "Comment"               // we need to tell Mongoose to expect an ObjectId and to tell it that its data comes from the Comment model.
+    }                             // The ref property is especially important because it tells the Pizza model which documents to search to find the right comments.
+  ],
+  toppings: [],
+},
+{
+  toJSON: {
+    virtuals: true,  //This adds the schema to be able to use the virtuals methods located at the bottom  
+  },
+  id: false         //We set id to false because this is a virtual that Mongoose returns, and we don’t need it.
+}
+);
+
+// get total count of comments and replies on retrieval / Needs toJSON property to the schema options as seen above.
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
 });
 
 const Pizza = model('Pizza', PizzaSchema);
